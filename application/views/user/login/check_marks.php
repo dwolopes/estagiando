66765a6e7d4ef793dc31663fc0d87f9be4a02aa0
Lopes <douglas.lopes@cromg.org.br>
2017-11-19 21:47:05 -0200

    Exibindo informações das marcações para o usuário.

--- application/views/user/login/check_marks.php
+++ application/views/user/login/check_marks.php
@@ -3,15 +3,39 @@
 	<div class="row">
 		<div class="col-md-12">
 			<div class="page-header">
-				<h1>Marcação realizada com sucesso!</h1>
+				<h1>Verifique as datas de sua marcação :)</h1>
 			</div>
-			<p>Marcações realizadas é um sinal de tranquilidade para todos nós :)</p>
+			<strong><p>Marcações realizadas é um sinal de tranquilidade para todos nós :)</p></strong>
+		</div>
+	</div><!-- .row -->
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+  				<!-- Default panel contents -->
+  				<div class="panel-heading">Histórico de marcações</div>
+  				<div class="panel-body">
+    				<p>Todas suas marcações realizadas estão representadas na tabela abaixo.</p>
+  				</div>
+  				<!-- Table -->
+  				<table class="table">
+  					<thead>
+  						<tr>
+  							<td><center><strong>Data<strong><center></td>
+  							<td><center><strong>Hora<strong><center></td>
+  							<td><center><strong>Tipo<strong><center></td>
+  						</tr>
+  					</thead>
+  					<tbody>
+  						<?php foreach ($marks as $item) {?>
+  							<tr>
+  								<td><center><?php echo $item['data']; ?></center></td>
+  								<td><center><?php echo $item['hora']; ?></center></td>
+  								<td><center><?php echo $item['tipo_marcacao']; ?></center></td>
+  							</tr>
+  						<?php } ?>
+  					</tbody>
+  				</table>
+			</div>
 		</div>
 	</div><!-- .row -->
 </div><!-- .container -->
-
-	<?php foreach ($marks as $item) {?>
-        <div class="list-group-item clearfix">
-          <?php echo $item['data']; ?>
-        </div>
-	<?php } ?>
